0f7e0f3e34275b1d2d9ceab6a73f8b4b0068a957
Ginery (Ginx) <songalingginery@gmail.com>
2024-05-23 17:14:00 +0800

    added models for documents

---
+++ app/Models/Document.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Document extends Authenticatable {
+    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'id',
+        'document_name',
+        'document_type',
+        'document_size',
+        'description',
+        'date_added',
+        'status', // 1 complete, 0 - in-progress, 2 - archived.
+        'user_added'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+}
