d69aeebd21a17191412f956e740a949212f64708
Ginery (Ginx) <songalingginery@gmail.com>
2024-05-23 19:17:34 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,7 +157,7 @@
             <!--begin::Nav-->
             <div class="navi navi-spacer-x-0 p-0">
                <!--begin::Item-->
-               <a href="custom/apps/user/profile-1/personal-information.html" class="navi-item">
+               <a href="{{route('profile.edit')}}" class="navi-item">
                   <div class="navi-link">
                      <div class="symbol symbol-40 bg-light mr-3">
                         <div class="symbol-label">
