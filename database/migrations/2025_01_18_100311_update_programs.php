6d3b87209e8715d576f19c044f33e106e3f698f1
Ginery (Ginx) <songalingginery@gmail.com>
2025-01-18 18:04:54 +0800

    migration updates

---
+++ database/migrations/2025_01_18_100311_update_programs.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('programs')) {
+            Schema::create('programs', function (Blueprint $table) {
+                $table->id();
+                $table->string('program_name');
+                $table->string('program_desc');
+                $table->longText('users_involve');
+                $table->integer('added_by');
+                $table->timestamps();
+            });
+        }else{
+            Schema::table('programs', function (Blueprint $table) {
+                // Check if the columns already exist before adding them
+                if (!Schema::hasColumn('programs', 'program_name')) {
+                    $table->string('program_name');
+                }
+                if (!Schema::hasColumn('programs', 'program_desc')) {
+                    $table->string('program_desc');
+                }
+                if (!Schema::hasColumn('programs', 'users_involve')) {
+                    $table->longText('users_involve');
+                }
+                if (!Schema::hasColumn('programs', 'added_by')) {
+                    $table->integer('added_by');
+                }              
+                if (!Schema::hasColumn('programs', 'created_at')) {
+                    $table->timestamp('created_at')->nullable();
+                }
+                if (!Schema::hasColumn('programs', 'updated_at')) {
+                    $table->timestamp('updated_at')->nullable();
+                }            
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
